61fabf7b768008758e7572cf3dec99f23b9d0bc3
Abu Jafar Md Jakaria <abujafar492@gmail.com>
2024-03-05 14:59:06 +0600

    add array_in_c & array user einput

---
+++ Array_in_c/userinput_.c
@@ -0,0 +1,18 @@
+///Taking array as input
+#include<stdio.h>
+
+int main()
+{
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    for(int i = 0; i < size; i++)
+        scanf("%d", &arr[i]);
+    ///Printing the array
+    printf("Printing the array: \n");
+    for(int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+
+
+
+}
